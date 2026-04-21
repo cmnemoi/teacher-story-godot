b8c74a58453ffa9fa9878c2d61d6d53f80c36b22
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-04-21 16:41:03 +0200

    fix camera bug

--- scenes/gamemain/mission_selection_screen.gd
+++ scenes/gamemain/mission_selection_screen.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var scroll_camera: Camera2D = $ScrollCamera
 @export var scroll_power : int = 20
+var base_cam_pos : Vector2
+
+func _ready():
+	base_cam_pos = scroll_camera.position
 
 func _process(_delta: float) -> void:
 	if visible:
@@ -9,3 +13,5 @@ func _process(_delta: float) -> void:
 			scroll_camera.position.y += scroll_power
 		elif Input.is_action_just_pressed("scroll_up"):
 			scroll_camera.position.y -= scroll_power
+	else:
+		scroll_camera.position = base_cam_pos
