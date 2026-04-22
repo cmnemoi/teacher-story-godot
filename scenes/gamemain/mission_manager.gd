030363402656a1f6168a6c5e1c6786979caae2f8
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-04-22 23:08:39 +0200

    rework mission conditions (still in progress)

--- scenes/gamemain/mission_manager.gd
+++ scenes/gamemain/mission_manager.gd
@@ -4,7 +4,7 @@ var current_mission_resource : MissionBase
 var rng = RandomNumberGenerator.new()
 const POSSIBLE_DIFFICULTIES : Array[int] = [0,1,2,3]
 const DIFFICULTIES_WEIGHTS : Array[int] = [7,13,10,5]
-const POSSIBLE_GOALS : Array[int] = [10,11,12,13,14,15,16]
+const POSSIBLE_GOALS : Array[int] = [8,9,10,11,12,13,14]
 const POSSIBLE_GOALS_FOR_X_ELEVES : Array[int] = [10,12,14,16]
 var goals_weights : Array[int] = [7,10,9,6,2,0]
 
@@ -57,47 +57,70 @@ func make_new_random_mission():
 				goals_weights = [2,10,5,0]
 			else:
 				goals_weights = [3,7,10,9,6,2,0]
-			new_resource.goal_is_for_x_student = randi_range(1,2)
-			new_resource.ideal_max_lesson = 6
-			new_resource.absolute_max_lesson = 9
 			new_resource.students = ManagerList.student_manager.generate_x_random_student(5)
-			new_resource.reward = 40
 		1:
 			if all_students:
 
 				goals_weights = [1,7,7,1]
 			else:
 				goals_weights = [2,4,8,9,5,2,0]
-			new_resource.goal_is_for_x_student = randi_range(1,3)
-			new_resource.ideal_max_lesson = 5
-			new_resource.absolute_max_lesson = 7
 			new_resource.students = ManagerList.student_manager.generate_x_random_student(5)
-			new_resource.reward = 60
 		2:
 			if all_students:
 				goals_weights = [0,4,12,4]
 			else:
 				goals_weights = [0,2,5,9,11,6,2]
-			new_resource.goal_is_for_x_student = randi_range(2,4)
-			new_resource.ideal_max_lesson = 4
-			new_resource.absolute_max_lesson = 6
 			new_resource.students = ManagerList.student_manager.generate_x_random_student(6)
-			new_resource.reward = 90
 		3:
 			if all_students:
 				goals_weights = [0,0,8,3]
 			else:
 				goals_weights = [0,0,2,4,7,6,2]
-			new_resource.goal_is_for_x_student = randi_range(3,5)
-			new_resource.ideal_max_lesson = 3
-			new_resource.absolute_max_lesson = 4
 			new_resource.students = ManagerList.student_manager.generate_x_random_student(8)
-			new_resource.reward = 120
 	
+
 	new_resource.goal = POSSIBLE_GOALS[rng.rand_weighted(goals_weights)]
+	if !all_students:
+		match new_resource.goal:
+			10:  new_resource.goal_is_for_x_student = randi_range(1,5)
+			12: new_resource.goal_is_for_x_student = randi_range(1,5)
+			14: new_resource.goal_is_for_x_student = randi_range(1,4)
+			16:  new_resource.goal_is_for_x_student = randi_range(1,3)
 	if randi_range(0,1000000) == 8:
 		new_resource.goal = 2 #Enjoy the free win, you earned it
 	new_resource.difficulty = difficulty
 	if all_students:
 		new_resource.goal_is_for_x_student = -1
+	
+	var best_students
+	var worst_student = StudentResource.new()
+	worst_student.note = 20
+	if all_students:
+		for student in new_resource.students:
+			if student.note < worst_student.note:
+				worst_student = student
+		new_resource.ideal_max_lesson = int(floor((new_resource.goal - worst_student.note)/2))+1 
+	else:
+		best_students = [] 
+		for i in range(new_resource.goal_is_for_x_student):
+			var best_student = StudentResource.new()
+			best_student.note = 0
+			for student in new_resource.students:
+				if student.note > best_student.note and new_resource.students not in best_students:
+					best_student = student
+			best_students.append(best_student)
+		for student in best_students:
+			if student.note < worst_student.note:
+				worst_student = student
+		new_resource.ideal_max_lesson = int(floor((new_resource.goal - worst_student.note)/2))+1 
+	
+	var student_sum = 0
+	if all_students:
+		for student in new_resource.students:
+			student_sum += (new_resource.goal-student.note) 
+	else:
+		for student in best_students:
+			student_sum += (new_resource.goal-student.note)
+	
+	new_resource.reward = 10* int(new_resource.ideal_max_lesson +  floor( student_sum / new_resource.ideal_max_lesson))
 	return new_resource
