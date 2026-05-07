a5b706ba03b705d321f2f30c92c989789aa504b9
SeldonHh <hadrien.heurteaux91@gmail.com>
2026-05-07 10:00:37 +0200

    Reworked skill and added swap

--- scenes/skills/skill_base.gd
+++ scenes/skills/skill_base.gd
@@ -3,6 +3,7 @@ extends Control
 @export var resource : SkillResource = preload("res://resource/Skills/Enseigner.tres")
 
 @onready var main_button: TextureButton = $MainButton
+var disabled := false
 
 func _ready() -> void:
 	Global.skill_list.append(self)
@@ -15,6 +16,7 @@ func _on_main_button_pressed() -> void:
 	for skill in Global.skill_list:
 		skill.main_button.disabled = true
 	var student_targets = []
+	var secondary_targets = []
 	match resource.target:
 		"Self": student_targets = []
 		"Column" : student_targets = await SkillTargetSelectHandler.select_column()
@@ -22,17 +24,35 @@ func _on_main_button_pressed() -> void:
 		"Table":
 			student_targets = await SkillTargetSelectHandler.select_groupdesk()
 		"All": student_targets = SkillTargetSelectHandler.students
+		"Two Students": 
+			student_targets = await SkillTargetSelectHandler.select_student()
+			student_targets.append( await SkillTargetSelectHandler.select_student())
+		"Student and Table":
+			student_targets = await  SkillTargetSelectHandler.select_student()
+			await get_tree().create_timer(.1).timeout
+			secondary_targets = await SkillTargetSelectHandler.select_desk()
 	for student in student_targets:
 		if student is Student:
 			student.damage(resource.damage_modifier,resource.ennui_breaker,resource.ennui_only)
 	match resource.name:
 		"Rappel à l'ordre": pass #TODO: Remove negative effect
 		"Concentration": pass #TODO: Add positive effect 
+		"Changement de place": ManagerList.desk_manager.assign_student_to_another_desk(student_targets[0],secondary_targets)
 	
+	resource.current_cooldown = resource.cooldown
 	for skill in Global.skill_list:
 		skill.main_button.disabled = false
-	ManagerList.timer_manager.update_time(-1)
-	ManagerList.teacher_manager.teacher_life -= randi_range(0,3)
+	if !resource.fast_skill:
+		ManagerList.timer_manager.update_time(-1)
+		ManagerList.teacher_manager.teacher_life -= randi_range(0,3)
+
+func _process(_delta: float) -> void:
+	disabled = resource.current_cooldown > 0
+	if disabled:
+		modulate = Color(0.53, 0.53, 0.53, 1.0)
+		main_button.disabled = true
+	else:
+		modulate = Color.WHITE
 
 func _on_mouse_entered() -> void:
 	$Tooltip.show()
